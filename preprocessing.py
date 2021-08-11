82d8219171db85ec3bf71c1bc12de0d1fe61c6e0
DonghaeSuh <gypsi12@naver.com>
2021-08-11 21:42:52 +0900

    preprocessing unfinished

--- preprocessing.py
+++ preprocessing.py
@@ -2,8 +2,8 @@ from gensim.corpora import WikiCorpus, Dictionary
 from gensim.corpora.wikicorpus import tokenize
 
 in_f ="/notebooks/embedding/data/raw/kowiki-latest-pages-articles.xml.bz2"
-out_f="/notebooks/embedding/data/processed.processed_wiki_ko.txt"
-output =open(out_f,'w', encoding ='utf-8')
+out_f="/notebooks/embedding/data/processed/processed_wiki_ko.txt"
+output = open(out_f,'w',encoding = 'utf-8')
 wiki =WikiCorpus(in_f,tokenizer_func=tokenize,dictionary=Dictionary())
 
 def main():
@@ -11,9 +11,10 @@ def main():
     for test in wiki.get_texts():
        output.wirte(bytes(''.join(text), 'utf-8').decode('utf-8')+'\n')
        i=i+1
-       if(i%1000==0):
+       if(i%10000==0):
            print('Processed'+str(i)+'articles')
        output.close()
        print('Processing complete!')
+
 if __name__ == "__main__":
-        main()      
+      main()     
