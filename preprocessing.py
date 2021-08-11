ba78b8e9711958d8321adeef638f2a8250e364a3
DonghaeSuh <gypsi12@naver.com>
2021-08-11 18:08:38 +0900

    first preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,19 @@
+from gensim.corpora import WikiCorpus, Dictionary
+from gensim.corpora.wikicorpus import tokenize
+
+in_f ="/notebooks/embedding/data/raw/kowiki-latest-pages-articles.xml.bz2"
+out_f="/notebooks/embedding/data/processed.processed_wiki_ko.txt"
+output =open(out_f,'w', encoding ='utf-8')
+wiki =WikiCorpus(in_f,tokenizer_func=tokenize,dictionary=Dictionary())
+
+def main():
+    i=0
+    for test in wiki.get_texts():
+       output.wirte(bytes(''.join(text), 'utf-8').decode('utf-8')+'\n')
+       i=i+1
+       if(i%1000==0):
+           print('Processed'+str(i)+'articles')
+       output.close()
+       print('Processing complete!')
+if __name__ == "__main__":
+        main()      
